8f5a66d3183582bff30ec7bc4e6839bfd27d30d1
“abeerscode” <md.abeerhasan73@gmail..com>
2026-02-08 02:27:40 +0600

    Problem 4

---
+++ ptc4.cpp
@@ -0,0 +1,11 @@
+// 4. User Input Output
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "I am " << name ;
+}
