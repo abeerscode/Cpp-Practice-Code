aac3e951da14cc9ac1dcaf75c6f0046a4d06f63a
“abeerscode” <md.abeerhasan71@gmail.com>
2026-02-08 02:07:42 +0600

    Problem 3

---
+++ ptc3.cpp
@@ -0,0 +1,19 @@
+// 3. Difference of two numbers
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+    int sum;
+
+    cout << "Enter number 1: ";
+    cin >> num1;
+    cout << "Enter number 2: ";
+    cin >> num2;
+
+    sum = num1 - num2;
+
+    cout << "Difference of two number is: " << sum;
+
+}
