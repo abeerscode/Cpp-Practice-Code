d6c2ff6d250adaaaae2186222004f6e8dfbe4e12
“abeerscode” <md.abeerhasan71@gmail.com>
2026-02-08 01:58:38 +0600

    Problem 2

--- ptc2.cpp
+++ ptc2.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main(){
-    cout << "Hello, World!" << endl;
-    return 0;
+    int num1, num2;
+    int sum;
+
+    cout << "Enter number 1: ";
+    cin >> num1;
+    cout << "Enter number 2: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "Summation of two number is: " << sum;
+
 }
