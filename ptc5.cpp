282d5efd00ac29d47fa1d4b58f0d87ead05a0032
“abeerscode” <md.abeerhasan73@gmail.com>
2026-02-08 02:31:42 +0600

    Problem 5

---
+++ ptc5.cpp
@@ -0,0 +1,19 @@
+// 3. Multiply two numbers
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+    int product;
+
+    cout << "Enter number 1: ";
+    cin >> num1;
+    cout << "Enter number 2: ";
+    cin >> num2;
+
+    product = num1 * num2;
+
+    cout << "Product of two number is: " << product;
+
+}
