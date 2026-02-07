a8c6ee32b5ae240b737ff769e0f23bc83af8a5eb
“abeerscode” <md.abeerhasan73@gmail.com>
2026-02-08 02:39:01 +0600

    Problem 6

---
+++ ptc6.cpp
@@ -0,0 +1,19 @@
+// 6. Division (integer & float)
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+    int result;
+
+    cout << "Enter number 1: ";
+    cin >> num1;
+    cout << "Enter number 2: ";
+    cin >> num2;
+
+    result = num1 / num2;
+
+    cout << "Division of two number is: " << result;
+
+}
